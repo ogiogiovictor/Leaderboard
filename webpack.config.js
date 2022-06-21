9536b09622933e532af414226e83860cf4fad6a0
OgiogioVictor <ogiogiovictor@gmail.com>
2022-06-21 13:39:53 -0600

    fixing the linters issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-      },
-      devtool: 'inline-source-map',
-      devServer: {
-        static: './dist',
-      },
-      plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-            template: './src/index.html',
-        }),
-      ],
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -34,7 +34,7 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: 'asset/resource',
       },
-     
+
     ],
   },
 };
